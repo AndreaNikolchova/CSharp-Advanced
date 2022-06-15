cc25998d6f853bba33ac5aa56cf4d15bf62d266a
AndreaNikolchova <a.nikolchova04@gmail.com>
2022-06-15 23:55:33 +0300

    Fixes!

--- Stacks and Queues/Balanced Parenthesis/Program.cs
+++ Stacks and Queues/Balanced Parenthesis/Program.cs
@@ -8,7 +8,58 @@ namespace Balanced_Parenthesis
     {
         static void Main(string[] args)
         {
-           
+            List<char> input = Console.ReadLine().ToCharArray().ToList();
+            Stack<char> stack = new Stack<char>();
+            foreach (var item in input)
+            {
+                if (item == '{' || item == '[' || item == '(')
+                    stack.Push(item);
+                else if (item == '}' && stack.Count>0)
+                {
+                    char braket = stack.Peek();
+                    if (braket == '{')
+                        stack.Pop();
+                    else
+                    {
+                        Console.WriteLine("NO");
+                        return;
+                    }
+                }
+                else if (item == ']'&&stack.Count > 0)
+                {
+                    char braket = stack.Peek();
+                    if (braket == '[')
+                        stack.Pop();
+                    else
+                    {
+                        Console.WriteLine("NO");
+                        return;
+                    }
+                }
+                else if(item == ')'&& stack.Count > 0)
+                {
+                    char braket = stack.Peek();
+                    if (braket == '(')
+                        stack.Pop();
+                    else
+                    {
+                        Console.WriteLine("NO");
+                        return;
+                    }
+                }
+               else
+                {
+                    Console.WriteLine("NO");
+                    return;
+                }    
+                  
+            }
+            if (stack.Count>0)
+            {
+                Console.WriteLine("NO");
+                return;
+            }
+            Console.WriteLine("YES");
         }
     }
 }
