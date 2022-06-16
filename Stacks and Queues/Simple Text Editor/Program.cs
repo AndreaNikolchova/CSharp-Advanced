f92c1746ddd3f970c04fbd91436a905811749f49
AndreaNikolchova <a.nikolchova04@gmail.com>
2022-06-16 15:31:30 +0300

    All

---
+++ Stacks and Queues/Simple Text Editor/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Simple_Text_Editor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            StringBuilder s = new StringBuilder();
+            Stack<string> stack = new Stack<string>();
+            for (int i = 1; i <= n; i++)
+            {
+                string[] command = Console.ReadLine().Split();
+                
+                switch (command[0])
+                {
+                    case "1":
+                        s.Append(command[1]);
+                        stack.Push(s.ToString());
+                        break;
+                    case "2":
+                        s.Remove(s.Length-int.Parse(command[1]), int.Parse(command[1]));
+                        stack.Push(s.ToString());
+                        break;
+                    case "3":
+                        Console.WriteLine(s[int.Parse(command[1])-1]);
+                        break;
+                    case "4":
+                        stack.Pop();
+                        s.Clear();
+                        if (stack.Count>0)
+                            s.Append(stack.Peek());
+                        else
+                            s.Clear();
+                        break;
+                }
+            }
+        }
+    }
+}
