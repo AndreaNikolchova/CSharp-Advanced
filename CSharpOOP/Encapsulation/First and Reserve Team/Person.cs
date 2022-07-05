a41cd93282d713f08561c071967558e8fd36a0c8
AndreaNikolchova <a.nikolchova04@gmail.com>
2022-07-06 00:27:02 +0300

    Encapsulation lab!

---
+++ CSharpOOP/Encapsulation/First and Reserve Team/Person.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+
+        private string firstName;
+        private string lastName;
+        private int age;
+        private decimal salary;
+        public string FirstName
+        {
+            get { return firstName; }
+            set
+            {
+                if (value.Length >= 3)
+                {
+                    this.firstName = value;
+                }
+                else
+                    throw new ArgumentException("First name cannot contain fewer than 3 symbols!");
+            }
+        }
+        public string LastName
+        {
+            get { return lastName; }
+            set
+            {
+                if (value.Length >= 3)
+                {
+                    this.lastName = value;
+                }
+                else
+                    throw new ArgumentException("Last name cannot contain fewer than 3 symbols!");
+            }
+        }
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                if (value > 0)
+                {
+                    this.age = value;
+                }
+                else
+                    throw new ArgumentException("Age cannot be zero or a negative integer!");
+            }
+        }
+        public decimal Salary
+        {
+            get { return salary; }
+            set
+            {
+                if (value >= 650)
+                {
+                    this.salary = value;
+                }
+                else
+                    throw new ArgumentException("Salary cannot be less than 650 leva!");
+            }
+        }
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+            this.Salary = salary;
+        }
+    }
+}
---
+++ CSharpOOP/Encapsulation/First and Reserve Team/Team.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonsInfo
+{
+    public class Team
+    {
+        private string name;
+        private List<Person> firstTeam;
+        private List<Person> reserveTeam;
+        public string Name 
+        { 
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                this.name = value;
+            }
+        }
+
+        public IReadOnlyCollection<Person> FirstTeam { get { return this.firstTeam.AsReadOnly(); } }
+        public IReadOnlyCollection<Person> ReserveTeam { get { return this.reserveTeam.AsReadOnly(); } }
+
+        public Team(string name)
+        {
+            this.Name = name;
+            this.firstTeam = new List<Person>();
+            this.reserveTeam = new List<Person>();
+        }
+        public void AddPlayer(Person person)
+        {
+            if (person.Age<40)
+            {
+                firstTeam.Add(person);
+            }
+            else
+            {
+                reserveTeam.Add(person);
+            }
+        }
+        public override string ToString()
+        {
+            return $"First team has {this.FirstTeam.Count} players.{Environment.NewLine}Reserve team has {this.ReserveTeam.Count} players.";
+        }
+    }
+}
