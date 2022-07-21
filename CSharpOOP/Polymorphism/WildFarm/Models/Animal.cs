cff21f1e75d0f59605b5342fbfe6c91298c6ff64
AndreaNikolchova <a.nikolchova04@gmail.com>
2022-07-21 12:48:32 +0300

    Last problem fixes

--- CSharpOOP/Polymorphism/WildFarm/Models/Animal.cs
+++ CSharpOOP/Polymorphism/WildFarm/Models/Animal.cs
@@ -6,15 +6,17 @@ namespace WildFarm.Models
 {
     public abstract class Animal
     {
-        protected Animal(string name,double weight,int foodEaten)
+        protected Animal(string name,double weight)
         {
             this.Name = name;
             this.Weight = weight;
-            this.FoodEaten = foodEaten;
+           
         }
             
         public string Name { get; set; }
         public double Weight { get; set; } 
         public int FoodEaten { get;set; }
+        public abstract string ProduceSound();
+        public abstract void Eat(Food food);
     }
 }
--- CSharpOOP/Polymorphism/WildFarm/StartUp.cs
+++ CSharpOOP/Polymorphism/WildFarm/StartUp.cs
@@ -1,10 +1,15 @@
 ﻿namespace WildFarm
 {
+    using IO;
+    using Core;
     public class StartUp
     {
         static void Main(string[] args)
         {
-           
+            IWriter writer = new Write();
+            IReader reader = new Read();
+            Engine engine = new Engine(writer,reader);
+            engine.Start();
         }
     }
 }
